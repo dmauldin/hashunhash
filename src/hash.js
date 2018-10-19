e6d06971b626c159bd2df0012ec89beb9e90843d
Dave Mauldin <dmauldin@gmail.com>
2018-10-19 11:40:13 -0700

    update some more comments

--- src/hash.js
+++ src/hash.js
@@ -24,8 +24,11 @@ function unhash(hsh = 0, len = 0) {
   let winner = "";
 
   // if no length is provided, let's at least take a guess
+  // (could also determine if the hash function is collision free and then this
+  // might not be a guess, at all)
   if (len < 1) {
     // iterate through lengths starting from 1 to see if we get any matches
+    // could be optimized to start with a min length based on the size of hsh
     for (let il = 1; il < maxGuessLength; il++) {
       const s = unhash(hv.toString(10), il);
       if (s.length > 0) {
@@ -34,7 +37,9 @@ function unhash(hsh = 0, len = 0) {
     }
   }
 
+  // iterate over the positions in the original string based on the given length
   for (let i = 0; i < len; i++) {
+    // iterate over the possible characters for this position in the original string
     for (let letterIndex = 0; letterIndex < letters.length; letterIndex++) {
       const letter = letters[letterIndex];
       // TODO: optimize by using a binary search instead of linearly finding the next letter
@@ -46,7 +51,7 @@ function unhash(hsh = 0, len = 0) {
         const low = (winner + letter).padEnd(len, "a");
         const lowHash = hash(low);
         if (hv.gte(lowHash)) {
-          // correct letter for result[i] found as letters[letterIndex]
+          // correct letter for winner[i] found as letters[letterIndex]
           winner += letter;
           break;
         }
