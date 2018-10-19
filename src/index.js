4214365e1e984bd989c63d347960f0ec93ce3daf
Dave Mauldin <dmauldin@gmail.com>
2018-10-19 11:26:06 -0700

    remove unused stylesheet

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import UnHasher from "./UnHasher";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { withStyles } from "@material-ui/core/styles";
-import "./styles.scss";
 
 const StyledPaper = withStyles({
   root: {
